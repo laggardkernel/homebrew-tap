f0f5e857c647c23167b8759f77dadf881b14eff6
laggardkernel <laggardkernel@gmail.com>
2019-03-14 16:55:23 +0800

    unbound: revert options removal

---
+++ Formula/unbound.rb
@@ -0,0 +1,90 @@
+class Unbound < Formula
+  desc "Validating, recursive, caching DNS resolver"
+  homepage "https://www.unbound.net"
+  url "https://nlnetlabs.nl/downloads/unbound/unbound-1.9.1.tar.gz"
+  sha256 "c3c0bf9b86ccba4ca64f93dd4fe7351308ab54293f297a67de5a8914c1dc59c5"
+  head "https://github.com/NLnetLabs/unbound.git"
+
+  bottle do
+    sha256 "89df414d18be41558471adda800315117044f68648e40d8457c788617b856466" => :mojave
+    sha256 "badbcd082ee047687ee5162846ffd72761041d5a88139800934a212d56806c94" => :high_sierra
+    sha256 "baf967dc36a36cd3f69c7502af0a22984abcd3a8853a279c89c8c6fe0051e977" => :sierra
+  end
+
+  deprecated_option "with-python" => "with-python@2"
+
+  depends_on "libevent"
+  depends_on "openssl"
+  depends_on "python@2" => :optional
+  depends_on "swig" if build.with? "python@2"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --with-libevent=#{Formula["libevent"].opt_prefix}
+      --with-ssl=#{Formula["openssl"].opt_prefix}
+    ]
+
+    if build.with? "python@2"
+      ENV.prepend "LDFLAGS", `python-config --ldflags`.chomp
+      ENV.prepend "PYTHON_VERSION", "2.7"
+
+      args << "--with-pyunbound"
+      args << "--with-pythonmodule"
+      args << "PYTHON_SITE_PKG=#{lib}/python2.7/site-packages"
+    end
+
+    args << "--with-libexpat=#{MacOS.sdk_path}/usr" if MacOS.sdk_path_if_needed
+    system "./configure", *args
+
+    inreplace "doc/example.conf", 'username: "unbound"', 'username: "@@HOMEBREW-UNBOUND-USER@@"'
+    system "make"
+    system "make", "test"
+    system "make", "install"
+  end
+
+  def post_install
+    conf = etc/"unbound/unbound.conf"
+    return unless conf.exist?
+    return unless conf.read.include?('username: "@@HOMEBREW-UNBOUND-USER@@"')
+
+    inreplace conf, 'username: "@@HOMEBREW-UNBOUND-USER@@"',
+                    "username: \"#{ENV["USER"]}\""
+  end
+
+  plist_options :startup => true
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-/Apple/DTD PLIST 1.0/EN" "http:/www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_sbin}/unbound</string>
+          <string>-d</string>
+          <string>-c</string>
+          <string>#{etc}/unbound/unbound.conf</string>
+        </array>
+        <key>UserName</key>
+        <string>root</string>
+        <key>StandardErrorPath</key>
+        <string>/dev/null</string>
+        <key>StandardOutPath</key>
+        <string>/dev/null</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system sbin/"unbound-control-setup", "-d", testpath
+  end
+end
