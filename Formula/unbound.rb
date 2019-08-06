0c52fc8e475a10b80353450f032a0fd9a3b5fa96
laggardkernel <laggardkernel@gmail.com>
2019-08-06 10:09:53 +0800

    unbound 1.9.2 revision 1

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -3,12 +3,13 @@ class Unbound < Formula
   homepage "https://www.unbound.net"
   url "https://nlnetlabs.nl/downloads/unbound/unbound-1.9.2.tar.gz"
   sha256 "6f7acec5cf451277fcda31729886ae7dd62537c4f506855603e3aa153fcb6b95"
+  revision 1
   head "https://github.com/NLnetLabs/unbound.git"
 
   bottle do
-    sha256 "3bb8f456b33fe54242f9df225e6eb6d9baa8e402f93159d8eec54abcd55032de" => :mojave
-    sha256 "8e3c5b954ec3d15a64aec681c604087945343e47630052fde70d2ab2c09dca79" => :high_sierra
-    sha256 "61371a71201d410bc4de1bdc32074ded726908a81d30dea88763d9d1230a57f3" => :sierra
+    sha256 "1e5a0656a5280f923f82aaa48374a0ac6e1562b0f315f0f980e3aade6dc6d083" => :mojave
+    sha256 "afad7dffd4fbe9c1b9e87b69882439e8abe1e107c0efdcb398c208ddaedb3301" => :high_sierra
+    sha256 "e62f07fda5af05f595645fb28309c4041722fb8e88324164ac2ceaf088a53a55" => :sierra
   end
 
   deprecated_option "with-python" => "with-python@2"
