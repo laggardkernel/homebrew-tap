9a2040736a585f7ab4ae904028d09e7cef604dec
laggardkernel <laggardkernel@gmail.com>
2019-08-31 21:45:54 +0800

    unbound 1.9.3

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -1,15 +1,14 @@
 class Unbound < Formula
   desc "Validating, recursive, caching DNS resolver"
   homepage "https://www.unbound.net"
-  url "https://nlnetlabs.nl/downloads/unbound/unbound-1.9.2.tar.gz"
-  sha256 "6f7acec5cf451277fcda31729886ae7dd62537c4f506855603e3aa153fcb6b95"
-  revision 1
+  url "https://nlnetlabs.nl/downloads/unbound/unbound-1.9.3.tar.gz"
+  sha256 "1b55dd9170e4bfb327fb644de7bbf7f0541701149dff3adf1b63ffa785f16dfa"
   head "https://github.com/NLnetLabs/unbound.git"
 
   bottle do
-    sha256 "1e5a0656a5280f923f82aaa48374a0ac6e1562b0f315f0f980e3aade6dc6d083" => :mojave
-    sha256 "afad7dffd4fbe9c1b9e87b69882439e8abe1e107c0efdcb398c208ddaedb3301" => :high_sierra
-    sha256 "e62f07fda5af05f595645fb28309c4041722fb8e88324164ac2ceaf088a53a55" => :sierra
+    sha256 "e4a175d3455d6dd751ba7d204b56940d5656b4b046bbeaaf244980a64aebc532" => :mojave
+    sha256 "26f015c9843fdf02463b5e3d1abd0d0db719d35438c4c2657861addac87d76e5" => :high_sierra
+    sha256 "453370364375c32863dbe33cf71dfc4e6868197beb8870463f9ede676e9eef28" => :sierra
   end
 
   deprecated_option "with-python" => "with-python@2"
