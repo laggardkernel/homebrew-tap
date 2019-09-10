c9cb743de4abd9b126f243075f2620d2a24a3e7e
laggardkernel <laggardkernel@gmail.com>
2019-09-10 21:23:06 +0800

    bing-wallpaper: enable service by default

--- Formula/bing-wallpaper.rb
+++ Formula/bing-wallpaper.rb
@@ -24,6 +24,8 @@ class BingWallpaper < Formula
   EOS
   end
 
+  plist_options :startup => true
+
   def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
