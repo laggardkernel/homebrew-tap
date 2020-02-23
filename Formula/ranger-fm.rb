14fc8d1c0adb0722f9d9e52d4073a2ec98da1223
laggardkernel <laggardkernel@gmail.com>
2020-02-23 18:06:43 +0800

    ranger-fm 1.9.3

--- Formula/ranger-fm.rb
+++ Formula/ranger-fm.rb
@@ -3,8 +3,8 @@ class RangerFm < Formula
 
   desc "Vim-like file manager"
   homepage "https://github.com/ranger/ranger"
-  url "https://files.pythonhosted.org/packages/6c/95/a5a211e35084ac72cd427702dcc13982ddfbd7145522f68fd15b366e4201/ranger-fm-1.9.2.tar.gz"
-  sha256 "0ec62031185ad1f40b9faebd5a2d517c8597019c2eee919e3f1c60ce466d8625"
+  url "https://files.pythonhosted.org/packages/7e/19/a7d9bb8646f87df5a3126fc385e45c04e55639c0576d31351083cb02f248/ranger-fm-1.9.3.tar.gz"
+  sha256 "9476ed1971c641f4ba3dde1b8b80387f0216fcde3507426d06871f9d7189ac5e"
   head "https://github.com/ranger/ranger.git"
 
   # Depends on python@2 for the time being, cause:
@@ -12,7 +12,7 @@ class RangerFm < Formula
   #    https://github.com/ranger/ranger/issues/1546
   # 2. Using python@2 instead of the system one is recommended by the community
   #    https://github.com/Homebrew/homebrew-core/issues/26287
-  depends_on "python@2"
+  depends_on "python"
 
   def install
     virtualenv_install_with_resources
