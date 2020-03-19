238ce9f9ad443d8f62e894b34d7c433bed557c1a
laggardkernel <laggardkernel@gmail.com>
2020-03-20 00:22:13 +0800

    rename libass as libass-options

--- Formula/libass-options.rb
+++ Formula/libass-options.rb
@@ -1,13 +1,15 @@
-class Libass < Formula
+class LibassOptions < Formula
   desc "Subtitle renderer for the ASS/SSA subtitle format"
   homepage "https://github.com/libass/libass"
   url "https://github.com/libass/libass/releases/download/0.14.0/libass-0.14.0.tar.xz"
   sha256 "881f2382af48aead75b7a0e02e65d88c5ebd369fe46bc77d9270a94aa8fd38a2"
   revision 1
 
+
   bottle do
     cellar :any
     rebuild 1
+    sha256 "31612c258eb45354a212ec42c240676b0b297f6a7ef7693b666f3986c14c3c26" => :catalina
     sha256 "adf25e0a4a61f098662952861b1103493f2be98a14975b1cdd27c8aab3a9603a" => :mojave
     sha256 "d3a3e4c2ff26d2a10991134bca875ecafcff6bc8abb193f3c78cb8c0cd57c779" => :high_sierra
     sha256 "028e53840dcad7fa8291fddacd46be8276578a3fa8c058b04975cf56a802101d" => :sierra
