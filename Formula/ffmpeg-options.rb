bb2fae11d59ecc62337435009395b32e742b3960
laggardkernel <laggardkernel@gmail.com>
2020-10-27 11:15:24 +0800

    ffmpeg: merge update from upstream
    - enable librav1e, 0255b4c
    - Add libxml2 support to ffmpeg for MPD conversion, 0cdb771
    - fix RuboCop style, 31797cc
    - remove unused texi2html dependency, b4571ec
    - fix build from source with libsrt, f497d4d

--- Formula/ffmpeg-options.rb
+++ Formula/ffmpeg-options.rb
@@ -6,7 +6,16 @@ class FfmpegOptions < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-4.3.1.tar.xz"
   sha256 "ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb"
+  # None of these parts are used by default, you have to explicitly pass `--enable-gp>
+  # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
+  license "GPL-2.0-or-later"
+  revision 2
   head "https://github.com/FFmpeg/FFmpeg.git"
+
+  livecheck do
+    url "https://ffmpeg.org/download.html"
+    regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle :unneeded
 
@@ -23,7 +32,6 @@ class FfmpegOptions < Formula
 
   depends_on "nasm" => :build
   depends_on "pkg-config" => :build
-  depends_on "texi2html" => :build
   depends_on "aom"
   depends_on "dav1d"
   depends_on "fontconfig"
@@ -40,6 +48,7 @@ class FfmpegOptions < Formula
   depends_on "opencore-amr"
   depends_on "openjpeg"
   depends_on "opus"
+  depends_on "rav1e"
   depends_on "rtmpdump"
   depends_on "rubberband"
   depends_on "sdl2"
@@ -55,7 +64,15 @@ class FfmpegOptions < Formula
   depends_on "xz"
 
   uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
   uses_from_macos "zlib"
+
+  # https://trac.ffmpeg.org/ticket/8760
+  # Remove in next release
+  patch do
+    url "https://github.com/FFmpeg/FFmpeg/commit/7c59e1b0f285cd7c7b35fcd71f49c5fd52cf9315.patch?full_index=1"
+    sha256 "1cbe1b68d70eadd49080a6e512a35f3e230de26b6e1b1c859d9119906417737f"
+  end
 
   depends_on "chromaprint" => :optional
   depends_on "fdk-aac" => :optional
@@ -92,6 +109,7 @@ class FfmpegOptions < Formula
       --enable-libdav1d
       --enable-libmp3lame
       --enable-libopus
+      --enable-librav1e
       --enable-librubberband
       --enable-libsnappy
       --enable-libsrt
@@ -103,6 +121,7 @@ class FfmpegOptions < Formula
       --enable-libwebp
       --enable-libx264
       --enable-libx265
+      --enable-libxml2
       --enable-libxvid
       --enable-lzma
       --enable-libfontconfig
@@ -159,7 +178,7 @@ class FfmpegOptions < Formula
     bin.install Dir["tools/*"].select { |f| File.executable? f }
 
     # Fix for Non-executables that were installed to bin/
-    mv bin/"python", pkgshare/"python", :force => true
+    mv bin/"python", pkgshare/"python", force: true
   end
 
   test do
