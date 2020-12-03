356d39b7d46fe4a4de990aab5fff9d4f681684a2
laggardkernel <laggardkernel@gmail.com>
2020-12-03 21:27:47 +0800

    ffmpeg: update 4.3.1_4, build head without patch

--- Formula/ffmpeg-options.rb
+++ Formula/ffmpeg-options.rb
@@ -5,13 +5,23 @@
 class FfmpegOptions < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-4.3.1.tar.xz"
-  sha256 "ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb"
   # None of these parts are used by default, you have to explicitly pass `--enable-gp>
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
-  revision 3
+  revision 4
   head "https://github.com/FFmpeg/FFmpeg.git"
+
+  stable do
+    url "https://ffmpeg.org/releases/ffmpeg-4.3.1.tar.xz"
+    sha256 "ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb"
+
+    # https://trac.ffmpeg.org/ticket/8760
+    # Remove in next release
+    patch do
+      url "https://github.com/FFmpeg/FFmpeg/commit/7c59e1b0f285cd7c7b35fcd71f49c5fd52cf9315.patch?full_index=1"
+      sha256 "1cbe1b68d70eadd49080a6e512a35f3e230de26b6e1b1c859d9119906417737f"
+    end
+  end
 
   livecheck do
     url "https://ffmpeg.org/download.html"
@@ -67,13 +77,6 @@ class FfmpegOptions < Formula
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
-
-  # https://trac.ffmpeg.org/ticket/8760
-  # Remove in next release
-  patch do
-    url "https://github.com/FFmpeg/FFmpeg/commit/7c59e1b0f285cd7c7b35fcd71f49c5fd52cf9315.patch?full_index=1"
-    sha256 "1cbe1b68d70eadd49080a6e512a35f3e230de26b6e1b1c859d9119906417737f"
-  end
 
   depends_on "chromaprint" => :optional
   depends_on "fdk-aac" => :optional
