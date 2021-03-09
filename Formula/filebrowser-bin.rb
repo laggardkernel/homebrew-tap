690a925921f24921cefa8f2f067846303440b166
laggardkernel <laggardkernel@gmail.com>
2021-03-09 17:55:38 +0800

    Add new formula filebrowser-bin, version 2.12.0

---
+++ Formula/filebrowser-bin.rb
@@ -0,0 +1,97 @@
+class FilebrowserBin < Formula
+  desc "Web File Browser"
+  homepage "https://github.com/filebrowser/filebrowser"
+  version "2.12.0"
+  url "https://github.com/filebrowser/filebrowser/releases/download/v#{version}/darwin-amd64-filebrowser.tar.gz"
+  sha256 "90c425347ef3e0e12daa54cbd588cdc69ef9088bde831d96ce77245efb79ba67"
+
+  bottle :unneeded
+
+  conflicts_with "filebrowser", :because => "same package"
+
+  def install
+    Dir.glob(["filebrowser*"]).each do |dst|
+      bin.install "#{dst}" => "filebrowser"
+    end
+
+    share_dst = "#{prefix}/share/filebrowser"
+    mkdir_p "#{share_dst}"
+    config_path = etc/"filebrowser"
+
+    # https://github.com/filebrowser/filebrowser/blob/master/.docker.json
+    root_dir = ENV["USER"]
+    root_dir = "/Users/#{root_dir}"
+    (buildpath/".filebrowser.json").write <<~EOS
+      {
+        "port": 8080,
+        "baseURL": "",
+        "address": "localhost",
+        "log": "stdout",
+        "database": "#{config_path}/filebrowser.db",
+        "root": "#{root_dir}"
+      }
+    EOS
+
+    # Save a copy of default config into share
+    cp ".filebrowser.json", "#{share_dst}/"
+
+    # Install/move conf into etc
+    [".filebrowser.json"].each do |dst|
+      dst_default = config_path/"#{dst}.default"
+      rm dst_default if dst_default.exist?
+      config_path.install dst
+    end
+  end
+
+  def post_install
+    (var/"log/filebrowser").mkpath
+    chmod 0755, var/"log/filebrowser"
+  end
+
+  def caveats; <<~EOS
+    By default, filebrowser listens on localhost:8080 with auth
+      username: admin
+      password: admin
+
+    Check detail usage at https://filebrowser.org/
+  EOS
+  end
+
+  # #{etc} is not supported here
+  plist_options :manual => "filebrowser -c /usr/local/etc/filebrowser/.filebrowser.json"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/filebrowser</string>
+        <string>-c</string>
+        <string>#{etc}/filebrowser/.filebrowser.json</string>
+      </array>
+      <key>WorkingDirectory</key>
+      <string>#{etc}/filebrowser</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/filebrowser/filebrowser.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/filebrowser/filebrowser.log</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <dict>
+        <key>SuccessfulExit</key>
+        <false/>
+      </dict>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system "#{bin}/filebrowser", "version"
+  end
+end
