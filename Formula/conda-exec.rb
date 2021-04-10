d0d7e74a11f97fd4105520459ac39a2cd29d7d7a
laggardkernel <laggardkernel@gmail.com>
2021-04-10 19:58:54 +0800

    conda-exec: support linux

--- Formula/conda-exec.rb
+++ Formula/conda-exec.rb
@@ -1,18 +1,40 @@
 class CondaExec < Formula
   desc "Hermetic conda executable in a sinlge file"
   homepage "https://repo.anaconda.com/pkgs/misc/conda-execs/"
-  url "https://repo.anaconda.com/pkgs/misc/conda-execs/conda-4.7.12-osx-64.exe"
-  sha256 "664985c0f85291a1be3aee3cd00d3a93dd29ffe191cacfd54e2c84443548a31c"
-  head "https://repo.anaconda.com/pkgs/misc/conda-execs/conda-latest-osx-64.exe"
+  version "4.7.12"
 
   bottle :unneeded
 
+  on_macos do
+    url "https://repo.anaconda.com/pkgs/misc/conda-execs/conda-#{version}-osx-64.exe"
+    sha256 "664985c0f85291a1be3aee3cd00d3a93dd29ffe191cacfd54e2c84443548a31c"
+  end
+  on_linux do
+    url "https://repo.anaconda.com/pkgs/misc/conda-execs/conda-#{version}-linux-64.exe"
+  end
+
+  head do
+    on_macos do
+      url "https://repo.anaconda.com/pkgs/misc/conda-execs/conda-latest-osx-64.exe"
+    end
+    on_linux do
+      url "https://repo.anaconda.com/pkgs/misc/conda-execs/conda-latest-linux-64.exe"
+    end
+  end
+
   def install
-    bin.install Dir["conda-*-osx-64.exe"][0] => "conda-exec"
+    Dir["conda*"].each do |i|
+      bin.install i => "conda-exec"
+      break
+    end
   end
 
   test do
     system bin/"conda-exec", "-h"
   end
-
 end
+# Ref
+# - https://github.com/conda/conda/issues/9401
+# - https://stackoverflow.com/a/54563455/5101148
+# - https://repo.anaconda.com/pkgs/misc/conda-execs/ (channel misc deprecated)
+# - https://anaconda.org/conda-forge/conda-standalone/files
