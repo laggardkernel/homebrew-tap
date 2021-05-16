e83c44126f570b28e9cfce0eea66e8343fc02652
laggardkernel <laggardkernel@gmail.com>
2021-05-16 13:11:14 +0800

    mosdns: upgrade to 1.8.6

--- Formula/mosdns.rb
+++ Formula/mosdns.rb
@@ -3,7 +3,7 @@ require "language/go"
 class Mosdns < Formula
   desc "Flexible forwarding DNS client"
   homepage "https://github.com/IrineSistiana/mosdns"
-  version "1.8.5"
+  version "1.8.6"
   license "GPL-3.0"
 
   livecheck do
