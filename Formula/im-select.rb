a82c23f11ce1115f7a4036b87c020be53cc41b4b
laggardkernel <laggardkernel@gmail.com>
2021-05-24 12:28:03 +0800

    feat: new formula im-select

---
+++ Formula/im-select.rb
@@ -0,0 +1,19 @@
+class ImSelect < Formula
+  desc "Switch your input method through terminal 📟"
+  homepage "https://github.com/daipeihust/im-select"
+  version "1.0.1"
+  url "https://raw.githubusercontent.com/daipeihust/im-select/#{version}/im-select-mac/out/im-select"
+  license "MIT"
+
+  bottle :unneeded
+
+  def install
+    # Xcode is needed for compiling to get the executable that is less than 30k.
+    # Skip building from the source, download the binary release directly.
+    bin.install "im-select"
+  end
+
+  test do
+    system "#{bin}/im-select"
+  end
+end
