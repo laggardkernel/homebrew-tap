0b676455eaf04641ec77b4e766e23dde6214a6ce
laggardkernel <laggardkernel@gmail.com>
2021-08-06 17:43:48 +0800

    feat(sensible-side-buttons): the forked one fixed the chrome compatible bug

---
+++ Casks/sensible-side-buttons.rb
@@ -0,0 +1,20 @@
+cask "sensible-side-buttons" do
+  version "1.08"
+  # sha256 ""
+
+  # Desousak's fork fixes compatibile problem with Chrome
+  # https://github.com/archagon/sensible-side-buttons/issues/58
+  url "https://github.com/Desousak/sensible-side-buttons/releases/download/#{version}/SensibleSideButtons.zip",
+      verified: "github.com/Desousak/sensible-side-buttons/"
+  name "Sensible Side Buttons"
+  homepage "https://sensible-side-buttons.archagon.net/"
+
+  conflicts_with cask: "sensiblesidebuttons"
+
+  app "SensibleSideButtons.app"
+
+  zap trash: [
+    "~/Library/Preferences/net.archagon.sensible-side-buttons.plist",
+    "~/Library/Preferences/net.archagon.sensible-side-buttons.plist.*",
+  ]
+end
