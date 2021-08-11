ded3e1037d4b759b562008c0b2c7ef265a40b30d
laggardkernel <laggardkernel@gmail.com>
2021-08-11 16:51:15 +0800

    feat: new formula besttrace (the command line version)

---
+++ Formula/besttrace.rb
@@ -0,0 +1,28 @@
+class Besttrace < Formula
+  desc "Enhanced traceroute with IP display, developed by IPIP.NET"
+  homepage "https://www.ipip.net/product/client.html#besttrace"
+  version "1.3.2"
+  url "https://cdn.ipip.net/17mon/besttrace4linux.zip"
+  # sha256 ""
+  license :cannot_represent
+
+  livecheck do
+    # Version provided in homepage doesn't match version of the pkg.
+    url "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=besttrace"
+    regex(%r{pkgver=v?(\d+(?:\.\d+)+(?:[-_].+?)?)}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).flatten.uniq
+    end
+  end
+
+  bottle :unneeded
+
+  def install
+    bin.install Dir.glob("besttrace*mac")[0] => "besttrace"
+    prefix.install Dir.glob("*.txt")[0] => "CHANGELOG.txt"
+  end
+
+  test do
+    system "#{bin}/besttrace", "--version"
+  end
+end
