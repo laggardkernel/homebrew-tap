48daef71b99e3b01d4bb25d004de7346d07e5901
laggardkernel <laggardkernel@gmail.com>
2021-08-16 14:52:08 +0800

    fix(aria2): bypass bugs in gnutls integration
    1. https://github.com/aria2/aria2/issues/1636
    
      Aria2 built with gnutls failed to use the gnutls provided ca certs.
      Switch to the system one during compiling with `--with-ca-bundle`.
    
    2. https://github.com/aria2/aria2/issues/1494
    
      Aria2 built with opessl failed to handshake with some certs.
      During my test, it failed to handshake with my self-signed cert
      cause only one ecdh curve secp256r1 was provided. But aria2 built
      with gnutls provides other curves and handshake succeeds.

--- Formula/aria2-options.rb
+++ Formula/aria2-options.rb
@@ -39,19 +39,22 @@ class Aria2Options < Formula
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --without-libgmp
-      --without-libgcrypt
       --with-libcares
       --with-libssh2
+      --without-libgmp
+      --without-libnettle
+      --without-libgcrypt
     ]
 
     if build.with? "gnutls"
       args << "--with-gnutls"
-      args << "--with-ca-bundle=#{etc}/gnutls/cert.pem"
+      # args << "--with-ca-bundle=#{etc}/gnutls/cert.pem"
+      # BUG: https://github.com/aria2/aria2/issues/1636
+      args << "--with-ca-bundle=/etc/ssl/cert.pem"
       args << "--without-appletls"
       args << "--without-openssl"
     elsif build.with? "openssl"
-      # aria2 failed to be built with openssl, both on Linux and macOS
+      # WARN: aria2 built with openssl has problem with some certs.
       # protocol error: https://github.com/aria2/aria2/issues/1494
       args << "--with-openssl"
       # ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl@1.1"].opt_lib}/pkgconfig"
@@ -59,12 +62,10 @@ class Aria2Options < Formula
       args << "--with-ca-bundle=#{etc}/openssl@1.1/cert.pem"
       args << "--without-appletls"
       args << "--without-gnutls"
-      args << "--without-libnettle"
     else
       args << "--with-appletls"
       args << "--without-openssl"
       args << "--without-gnutls"
-      args << "--without-libnettle"
     end
 
     system "./configure", *args
@@ -73,11 +74,19 @@ class Aria2Options < Formula
     bash_completion.install "doc/bash_completion/aria2c"
   end
 
+  def caveats
+    <<~EOS
+    Arai2 built with openssl has problem handshaking with some certs.
+    Reason unknown: https://github.com/aria2/aria2/issues/1494
+    EOS
+  end
+
   test do
     system "#{bin}/aria2c", "https://brew.sh/"
     assert_predicate testpath/"index.html", :exist?, "Failed to create index.html!"
   end
 end
 
-# ref
+# Ref
+# https://github.com/Homebrew/homebrew-core/pull/32328
 # - http://aria2.github.io/manual/en/html/README.html#dependency
