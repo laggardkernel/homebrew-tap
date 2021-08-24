ea9a92ce47bad58bc4052176c958ec8d280e6eb6
laggardkernel <laggardkernel@gmail.com>
2021-08-24 11:23:17 +0800

    Upgrade filebrowser to 2.17.1

--- Formula/filebrowser.rb
+++ Formula/filebrowser.rb
@@ -1,7 +1,7 @@
 class Filebrowser < Formula
   desc "Provides a file managing interface within a specified directory"
   homepage "https://filebrowser.org"
-  version "2.16.1"
+  version "2.17.1"
   # sha256
   license "MIT"
 
