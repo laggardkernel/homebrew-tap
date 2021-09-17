1909b7e41f699e10ca4f979f49cb4ec528672f9b
laggardkernel <laggardkernel@gmail.com>
2021-09-17 13:34:39 +0800

    New shell script osc52

---
+++ Formula/osc52.rb
@@ -0,0 +1,39 @@
+require "base64"
+
+class Osc52 < Formula
+  desc "Send an arbitrary string to the terminal clipboard using the OSC 52 escape sequence"
+  homepage "https://chromium.googlesource.com/apps/libapps/+log/master/hterm/etc/osc52.sh"
+  version "250bcf7"
+  url "https://chromium.googlesource.com/apps/libapps/+/#{version}/hterm/etc/osc52.sh?format=TEXT"
+  # sha256 ""
+  license :cannot_represent
+
+  livecheck do
+    url :homepage
+    regex(%r{href="/apps/libapps/\+/([a-z0-9]{7,}+)"}i)
+    strategy :page_match do |page, regex|
+      # Only return the 1st commit to avoid alphabetical version comparison
+      page.scan(regex).flatten.first&.slice!(0..6)
+    end
+  end
+
+  def install
+    File.open("osc52.sh", "rt") do |f|
+      content = f.read
+      File.open("osc52", "wb") do |f|
+        f.write(Base64.decode64(content))
+      end
+    end
+
+    bin.install "osc52"
+  end
+
+  test do
+    system "#{bin}/osc52", "--help"
+  end
+end
+
+# Ref
+# - https://github.com/google/gitiles/issues/7, unable to download raw files
+#   from googlesource
+# curl -sSL https://chromium.googlesource.com/apps/libapps/+/master/hterm/etc/osc52.sh | base64 -D >| osc52
