e4af1e17708b858cae9eb7223e4ca3a8cf0c00c3
laggardkernel <laggardkernel@gmail.com>
2021-10-02 09:54:49 +0800

    feat(version): Upgrade lua-language-server to 2.4.1

--- Formula/lua-language-server.rb
+++ Formula/lua-language-server.rb
@@ -1,7 +1,7 @@
 class LuaLanguageServer < Formula
   desc "Language Server for Lua and coded by Lua, the sumneko.lua ext for VSCode"
   homepage "https://github.com/sumneko/lua-language-server"
-  version "2.3.7"
+  version "2.4.1"
   license "MIT"
 
   livecheck do
