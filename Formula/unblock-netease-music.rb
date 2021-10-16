c7e6dca4f0fad04850ba4d6b008f98ca6f877574
laggardkernel <laggardkernel@gmail.com>
2021-10-16 20:25:29 +0800

    feat(unblock-netease-music): tweak select strategy
    Relax time difference in selection to 7s.

--- Formula/unblock-netease-music.rb
+++ Formula/unblock-netease-music.rb
@@ -1,31 +1,50 @@
 class UnblockNeteaseMusic < Formula
   desc "Revive unavailable songs for Netease Cloud Music"
   # homepage "https://github.com/nondanee/UnblockNeteaseMusic"
-  homepage "https://github.com/1715173329/UnblockNeteaseMusic"
   version "0.26.1"
-  url "https://github.com/1715173329/UnblockNeteaseMusic/archive/refs/tags/v#{version}.tar.gz"
+  # homepage "https://github.com/1715173329/UnblockNeteaseMusic"
+  homepage "https://github.com/UnblockNeteaseMusic/server"
+  url "https://github.com/UnblockNeteaseMusic/server/archive/refs/tags/v#{version}.tar.gz"
   # sha256 ""
   license "MIT"
 
   livecheck do
-    url :stable
-    strategy :github_latest
+    # Pre-release support
+    url "https://github.com/UnblockNeteaseMusic/server/releases"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+[-]?[^"]*)["' >]}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match&.first }
+    end
+    # url :stable
+    # strategy :github_latest
   end
 
   bottle :unneeded
 
   depends_on "node"
+  # depends_on "yarn" => :build # install dep and run with DEVELOPMENT=true
+  # Default yarn cache dir: #{buildpath}/.brew_home/Library/Caches/Yarn/v6
 
   def install
+    inreplace "src/provider/select.js" do |s|
+      s.gsub! "5 * 1e3", "8 * 1e3"
+    end
+    # Starting 0.27, precompiled entries are included to avoid yarn install
+    if File.exist? "precompiled/app.js"
+      inreplace "precompiled/app.js" do |s|
+        s.gsub! "5 * 1e3", "8 * 1e3"
+      end
+    end
+
     mkdir_p buildpath/"bin"
     (buildpath/"bin/unblock-nm").write <<~EOS
     #!/bin/sh
-    #{HOMEBREW_PREFIX}/opt/node/bin/node "#{prefix}/app.js" "$@"
+    #{HOMEBREW_PREFIX}/opt/node/bin/node "#{opt_prefix}/app.js" "$@"
     EOS
 
     (buildpath/"bin/unblock-nm-bridge").write <<~EOS
     #!/bin/sh
-    #{HOMEBREW_PREFIX}/opt/node/bin/node "#{prefix}/bridge.js" "$@"
+    #{HOMEBREW_PREFIX}/opt/node/bin/node "#{opt_prefix}/bridge.js" "$@"
     EOS
 
     bin.install
@@ -41,7 +60,7 @@ class UnblockNeteaseMusic < Formula
 
   def caveats
     <<~EOS
-    Fork on https://github.com/1715173329/UnblockNeteaseMusic.
+    Fork on https://github.com/UnblockNeteaseMusic/server
     Service listens on 16300, 16301 by default. Options are written in
 
       #{plist_path}
@@ -52,6 +71,8 @@ class UnblockNeteaseMusic < Formula
 
   plist_options manual: "unblock-nm --help"
 
+  # TODO: ANSI escape color code is not filtered when dumping log to file.
+  #  Switch json format log temporarily.
   def plist
     <<~EOS
       <?xml version="1.0" encoding="UTF-8"?>
@@ -60,6 +81,13 @@ class UnblockNeteaseMusic < Formula
         <dict>
           <key>Label</key>
           <string>#{plist_name}</string>
+          <key>EnvironmentVariables</key>
+          <dict>
+            <key>ENABLE_LOCAL_VIP</key>
+            <string>true</string>
+            <key>JSON_LOG</key>
+            <string>true</string>
+          </dict>
           <key>ProgramArguments</key>
             <array>
               <string>#{HOMEBREW_PREFIX}/opt/node/bin/node</string>
@@ -74,7 +102,6 @@ class UnblockNeteaseMusic < Formula
               <string>https://music.163.com</string>
               <string>-o</string>
               <string>kuwo</string>
-              <string>netease</string>
             </array>
             <key>RunAtLoad</key>
             <true/>
@@ -83,7 +110,7 @@ class UnblockNeteaseMusic < Formula
             <key>StandardOutPath</key>
             <string>#{var}/log/#{name}/access.log</string>
             <key>StandardErrorPath</key>
-            <string>#{var}/log/#{name}/error.log</string>
+            <string>#{var}/log/#{name}/access.log</string>
             <key>WorkingDirectory</key>
             <string>#{opt_prefix}</string>
           </dict>
