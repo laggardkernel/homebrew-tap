0e1a6a704f72398555999d8d30e60a0f18ec49d8
laggardkernel <laggardkernel@gmail.com>
2021-11-04 21:09:58 +0800

    filebrowser: 2.8.0

--- Formula/filebrowser.rb
+++ Formula/filebrowser.rb
@@ -1,7 +1,7 @@
 class Filebrowser < Formula
   desc "Provides a file managing interface within a specified directory"
   homepage "https://filebrowser.org"
-  version "2.17.2"
+  version "2.18.0"
   # sha256
   license "MIT"
 
