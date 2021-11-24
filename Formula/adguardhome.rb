bae938b5895cfdd03ccb049e7c4746e44a2ce4d8
laggardkernel <laggardkernel@gmail.com>
2021-11-24 16:25:50 +0800

    adguardhome: upgrade to 0.107.0-b.15

--- Formula/adguardhome.rb
+++ Formula/adguardhome.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Adguardhome < Formula
   desc "Network-wide ads & trackers blocking DNS server"
   homepage "https://github.com/AdguardTeam/AdGuardHome"
-  version "0.107.0-b.14"
+  version "0.107.0-b.15"
   license "GPL-3.0"
 
   livecheck do
