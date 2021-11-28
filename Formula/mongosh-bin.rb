690495d27d6201c1f5e21ed9bd2db3b559f0aaee
laggardkernel <laggardkernel@gmail.com>
2021-11-28 15:13:44 +0800

    mongosh-bin: update to 1.1.4

--- Formula/mongosh-bin.rb
+++ Formula/mongosh-bin.rb
@@ -1,7 +1,7 @@
 class MongoshBin < Formula
   desc "MongoDB Shell to connect, configure, query, and work with your MongoDB database"
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  version "1.1.2"
+  version "1.1.4"
   license "Apache-2.0"
 
   livecheck do
