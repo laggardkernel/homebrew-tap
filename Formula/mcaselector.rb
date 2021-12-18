a0dd4873a4d60a82b2447a737c6389ac20a23b2e
laggardkernel <laggardkernel@gmail.com>
2021-12-18 20:11:41 +0800

    update mongosh, mcaselector

--- Formula/mcaselector.rb
+++ Formula/mcaselector.rb
@@ -1,10 +1,10 @@
 class Mcaselector < Formula
   desc "Tool to select chunks from Minecraft worlds for deletion or export"
   homepage "https://github.com/toolbox4minecraft/amidst"
-  version "1.17.1"
+  version "1.17.2"
   url "https://github.com/Querz/mcaselector/releases/download/#{version}/mcaselector-#{version}.jar"
   license "GPL-3.0"
-  revision 1
+  revision 0
 
   def pkg_name
     "mcaselector"
