f8e9265681ed6bcad986f1b99b3f81c6623301d7
laggardkernel <laggardkernel@gmail.com>
2021-12-31 11:21:03 +0800

    update adguardhome, lua-language-server

--- Formula/lua-language-server.rb
+++ Formula/lua-language-server.rb
@@ -1,7 +1,7 @@
 class LuaLanguageServer < Formula
   desc "Language Server for Lua and coded by Lua, the sumneko.lua ext for VSCode"
   homepage "https://github.com/sumneko/lua-language-server"
-  version "2.5.5"
+  version "2.5.6"
   license "MIT"
 
   livecheck do
