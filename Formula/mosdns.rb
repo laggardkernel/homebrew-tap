a4f5a17f720df6778be2de0f39067a6e729898f2
laggardkernel <laggardkernel@gmail.com>
2022-01-05 10:38:13 +0800

    mosdns: update to 3.0.0

--- Formula/mosdns.rb
+++ Formula/mosdns.rb
@@ -1,7 +1,7 @@
 class Mosdns < Formula
   desc "Flexible forwarding DNS client"
   homepage "https://github.com/IrineSistiana/mosdns"
-  version "2.2.2"
+  version "3.0.0"
   license "GPL-3.0"
 
   head do
