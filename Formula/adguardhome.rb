002f5f64db15b91777cd963e4312db618f7328d0
laggardkernel <laggardkernel@gmail.com>
2022-03-03 09:55:50 +0800

    Update adguardhome, mongosh, nali

--- Formula/adguardhome.rb
+++ Formula/adguardhome.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Adguardhome < Formula
   desc "Network-wide ads & trackers blocking DNS server"
   homepage "https://github.com/AdguardTeam/AdGuardHome"
-  version "0.107.3"
+  version "0.107.4"
   license "GPL-3.0"
 
   livecheck do
