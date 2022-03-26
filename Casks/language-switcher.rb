9c5e30092d90e1b493b47d64fb56bca5ec21a955
laggardkernel <laggardkernel@gmail.com>
2022-03-26 17:26:13 +0800

    fix: disable language-switcher, rsyncosx-versions on Catalina+

--- Casks/language-switcher.rb
+++ Casks/language-switcher.rb
@@ -3,11 +3,14 @@ cask 'language-switcher' do
   sha256 'c65882f00b195a0821dd3baf2c81a71d3ddd01b64cf6beaf56abb47cb948ffa8'
 
   # Website unmaintained, get the app from the web
-  url "https://feng-bbs-att-1255531212.file.myqcloud.com/2013/03/13/4671851_Language_Switcher_1_1_7.dmg"
+  url "https://pseudocold.com/app/language-switcher/language-switcher-#{verison}.dmg"
+  # url "https://feng-bbs-att-1255531212.file.myqcloud.com/2013/03/13/4671851_Language_Switcher_1_1_7.dmg"
   # url "http://www.tj-hd.co.uk/downloads/Language_Switcher_#{version.dots_to_underscores}.dmg"
   appcast 'http://feeds.tj-hd.co.uk/feeds/language_switcher/appcast.xml'
   name 'Language Switcher'
   homepage 'http://www.tj-hd.co.uk/en-gb/languageswitcher/'
+
+  depends_on macos: "< :catalina"
 
   livecheck do
     skip "App unmaintained"
--- Casks/rsyncosx-versions.rb
+++ Casks/rsyncosx-versions.rb
@@ -10,10 +10,6 @@ cask "rsyncosx-versions" do
     version "6.5.8"
     sha256 "4f23be29b260a93e05a2b9abf2ac42419b918b455e664d12ed92663ee008ad4e"
     url "https://github.com/rsyncOSX/RsyncOSX/releases/download/v#{version}/RsyncOSX.#{version}.dmg"
-  else
-    version "6.6.4"
-    sha256 "924f058b7233aa1304a0e98c19ee6c294e8bb033842c26162a16165dd1ec896f"
-    url "https://github.com/rsyncOSX/RsyncOSX/releases/download/v#{version}/RsyncOSX.#{version}.dmg"
   end
 
   livecheck do
@@ -33,6 +29,8 @@ cask "rsyncosx-versions" do
   name "RsyncOSX"
   desc "GUI for rsync"
   homepage "https://github.com/rsyncOSX/RsyncOSX"
+
+  depends_on macos: "<= :catalina"
 
   app "RsyncOSX.app"
 
