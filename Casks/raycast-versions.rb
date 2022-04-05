6926202d9d5045434de69165673afb4099cac57d
laggardkernel <laggardkernel@gmail.com>
2022-04-05 20:43:12 +0800

    fix(raycast): livecheck

--- Casks/raycast-versions.rb
+++ Casks/raycast-versions.rb
@@ -2,21 +2,23 @@ cask "raycast-versions" do
   if MacOS.version <= :mojave
     version "1.26.3"
     url "https://pseudocold.com/app/raycast/#{version}/Raycast_v#{version}_x86.dmg"
+    livecheck do
+      skip "1.26.3, last version compatible with Mojave"
+    end
   else
     version "latest"
     sha256 :no_check
     url "https://api.raycast.app/v2/download"
+    livecheck do
+      url :url
+      strategy :header_match
+      regex(/Raycast[._-]v?(\d+(?:\.\d+)+)[._-]universal\.dmg/i)
+    end
   end
 
   name "Raycast"
   desc "Control your tools with a few keystrokes"
   homepage "https://raycast.app/"
-
-  livecheck do
-    url :url
-    strategy :header_match
-    regex(/Raycast[._-]v?(\d+(?:\.\d+)+)[._-]universal\.dmg/i)
-  end
 
   auto_updates true
   depends_on macos: ">= :mojave"
