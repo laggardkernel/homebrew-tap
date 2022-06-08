492af9f1881092bedb685cfe06614cc758dfd254
laggardkernel <laggardkernel@gmail.com>
2022-06-08 09:27:11 +0800

    Upgrade adguardhome to 0.107.7, clash, mosdns, dnslookup, ...

--- Formula/adguardhome.rb
+++ Formula/adguardhome.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Adguardhome < Formula
   desc "Network-wide ads & trackers blocking DNS server"
   homepage "https://github.com/AdguardTeam/AdGuardHome"
-  version "0.107.6"
+  version "0.107.7"
   license "GPL-3.0"
 
   livecheck do
--- Formula/mosdns.rb
+++ Formula/mosdns.rb
@@ -1,7 +1,7 @@
 class Mosdns < Formula
   desc "Flexible forwarding DNS client"
   homepage "https://github.com/IrineSistiana/mosdns"
-  version "3.8.0"
+  version "3.9.0"
   license "GPL-3.0"
 
   head do
