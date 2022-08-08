3494ab6ea9dbd1e541ca0f9eec75d66803e49831
laggardkernel <laggardkernel@gmail.com>
2022-08-08 09:55:04 +0800

    Bump formulae versions

--- Formula/ffmpeg-options.rb
+++ Formula/ffmpeg-options.rb
@@ -1,5 +1,5 @@
 class FfmpegOptions < Formula
-  version "5.0.1"
+  version "5.1"
   # sha256 ""
 
   desc "Play, record, convert, and stream audio and video"
@@ -174,15 +174,6 @@ class FfmpegOptions < Formula
       args << "--enable-decklink"
       args << "--extra-cflags=-I#{include}"
       args << "--extra-ldflags=-L#{include}"
-    end
-
-    # Replace hardcoded default VMAF model path
-    unless build.head?
-      %w[doc/filters.texi libavfilter/vf_libvmaf.c].each do |f|
-        inreplace f, "/usr/local/share/model", HOMEBREW_PREFIX/"share/libvmaf/model"
-        # Since libvmaf v2.0.0, `.pkl` model files have been deprecated in favor of `.json` model files.
-        inreplace f, "vmaf_v0.6.1.pkl", "vmaf_v0.6.1.json"
-      end
     end
 
     system "./configure", *args
--- Formula/mosdns.rb
+++ Formula/mosdns.rb
@@ -1,7 +1,7 @@
 class Mosdns < Formula
   desc "Flexible forwarding DNS client"
   homepage "https://github.com/IrineSistiana/mosdns"
-  version "4.1.8"
+  version "4.1.9"
   license "GPL-3.0"
 
   head do
