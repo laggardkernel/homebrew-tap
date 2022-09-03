c3ecc919997248e42ae41c14379031f790741e45
laggardkernel <laggardkernel@gmail.com>
2022-09-03 15:58:01 +0800

    Weekl upgrade

--- Formula/clash-premium.rb
+++ Formula/clash-premium.rb
@@ -1,7 +1,7 @@
 class ClashPremium < Formula
   desc "Rule-based tunnel in Go, the pre-built premium version"
   homepage "https://github.com/Dreamacro/clash/releases/tag/premium"
-  version "2022.07.07"
+  version "2022.08.26"
   license "GPL-3.0"
 
   livecheck do
