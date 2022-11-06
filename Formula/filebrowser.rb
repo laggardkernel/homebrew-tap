3e2eb840dfb2f0d5c06bfbec686bee9268a44d4a
laggardkernel <laggardkernel@gmail.com>
2022-11-06 16:56:55 +0800

    Upgrade filebrowser, hmcl

--- Formula/filebrowser.rb
+++ Formula/filebrowser.rb
@@ -1,7 +1,7 @@
 class Filebrowser < Formula
   desc "Provides a file managing interface within a specified directory"
   homepage "https://filebrowser.org"
-  version "2.22.4"
+  version "2.23.0"
   # sha256
   license "MIT"
 
