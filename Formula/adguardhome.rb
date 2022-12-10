4bd17562175c23d88e66098913223876a04b2ac7
laggardkernel <laggardkernel@gmail.com>
2022-12-10 14:38:21 +0800

    Upgrade adguardhome, ffmpeg, hmcl, mongosh, m3u8dl

--- Formula/adguardhome.rb
+++ Formula/adguardhome.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Adguardhome < Formula
   desc "Network-wide ads & trackers blocking DNS server"
   homepage "https://github.com/AdguardTeam/AdGuardHome"
-  version "0.107.19"
+  version "0.107.20"
   license "GPL-3.0"
 
   livecheck do
