e1b3a5e134a29525cf803514a3172db3e02897fa
laggardkernel <laggardkernel@gmail.com>
2023-04-07 10:06:50 +0800

    fix(clashx-meta): fix cask syntax

--- Casks/clashx-meta.rb
+++ Casks/clashx-meta.rb
@@ -1,7 +1,6 @@
 cask "clashx-meta" do
   version "1.2.3"
   # sha256
-  revision 1
 
   url "https://github.com/MetaCubeX/ClashX.Meta/releases/download/v#{version}/ClashX.Meta.zip"
   name "ClashX Meta"
