681dcb12a8ad3d83a3be33894f32399e4d2c9e7d
laggardkernel <laggardkernel@gmail.com>
2023-06-12 10:36:37 +0800

    Weekly version bump up for adguardhome, clashx-meta, ...

--- Formula/adguardhome.rb
+++ Formula/adguardhome.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Adguardhome < Formula
   desc "Network-wide ads & trackers blocking DNS server"
   homepage "https://github.com/AdguardTeam/AdGuardHome"
-  version "0.107.29"
+  version "0.107.31"
   license "GPL-3.0"
 
   livecheck do
