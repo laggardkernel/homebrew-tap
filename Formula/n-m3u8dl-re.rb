0db7b9d5e82d8a2afdcc6a9a6763bf2d8da3580f
laggardkernel <laggardkernel@gmail.com>
2023-06-19 10:32:58 +0800

    Weekly version bump up

--- Formula/n-m3u8dl-re.rb
+++ Formula/n-m3u8dl-re.rb
@@ -1,7 +1,7 @@
 class NM3u8dlRe < Formula
   desc "Cross-Platform stream downloader for DASH/HLS"
   homepage "https://github.com/nilaoda/N_m3u8DL-RE"
-  version "0.1.8-beta,20230615"
+  version "0.1.9-beta,20230618"
   license "MIT"
 
   livecheck do
