f7c51de913581d8643924e06262e766e0ea9ed19
laggardkernel <laggardkernel@gmail.com>
2023-09-08 11:23:40 +0800

    Weekly version bump up

--- Formula/unblock-netease-music.rb
+++ Formula/unblock-netease-music.rb
@@ -3,11 +3,11 @@ class UnblockNeteaseMusic < Formula
   # homepage "https://github.com/nondanee/UnblockNeteaseMusic"
   # homepage "https://github.com/1715173329/UnblockNeteaseMusic"
   homepage "https://github.com/UnblockNeteaseMusic/server"
-  version "0.27.3"
+  version "0.27.4"
   url "https://github.com/UnblockNeteaseMusic/server/archive/refs/tags/v#{version}.tar.gz"
   # sha256 ""
   license "MIT"
-  revision 1
+  revision 0
 
   livecheck do
     # Pre-release support
