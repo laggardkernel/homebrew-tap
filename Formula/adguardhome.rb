828670851032b94cbd2cb87f63e7be2338edf2b6
laggardkernel <laggardkernel@gmail.com>
2023-09-12 14:13:33 +0800

    Upgrade adguardhome 0.107.38, gitui 0.24.3

--- Formula/adguardhome.rb
+++ Formula/adguardhome.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Adguardhome < Formula
   desc "Network-wide ads & trackers blocking DNS server"
   homepage "https://github.com/AdguardTeam/AdGuardHome"
-  version "0.107.37"
+  version "0.107.38"
   license "GPL-3.0"
 
   livecheck do
