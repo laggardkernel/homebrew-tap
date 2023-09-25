2aaa0fa44c6d9b3c512d8b5f1a536ff136755871
laggardkernel <laggardkernel@gmail.com>
2023-09-25 22:49:44 +0800

    feat(openvpn-service): custom launchd conf for openvpn
    with log enabled

---
+++ Formula/openvpn-service.rb
@@ -0,0 +1,36 @@
+class OpenvpnService < Formula
+  desc "Custom launchd file for OpenVPN 2.x with log file enabled."
+  homepage "https://openvpn.net/community/"
+  version "0.1.0"
+  url "https://httpbin.org/anything/openvpn-service-#{version}"
+  # sha256
+
+  livecheck do
+    skip "Plist service, no version needed"
+  end
+
+  depends_on "openvpn"
+
+  def install
+    system "touch", "placeholder"
+    share.install "placeholder"
+  end
+
+  def post_install
+    (var/"log/openvpn").mkpath
+    chmod 0755, var/"log/openvpn"
+  end
+
+  service do
+    run ["#{HOMEBREW_PREFIX}/sbin/openvpn", "--config", etc/"openvpn/openvpn.conf"]
+    keep_alive true
+    require_root true
+    working_dir etc/"openvpn"
+    log_path var/"log/openvpn/openvpn.log"
+    error_log_path var/"log/openvpn/openvpn.log"
+  end
+
+  test do
+    system sbin/"openvpn", "--show-ciphers"
+  end
+end
