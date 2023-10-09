fdddc22ebc62a907f3ad6edf85d49f93b02a69e2
laggardkernel <laggardkernel@gmail.com>
2023-10-09 09:31:53 +0800

    Upgrade mosdns to 5.2.1

--- Formula/mosdns.rb
+++ Formula/mosdns.rb
@@ -1,7 +1,7 @@
 class Mosdns < Formula
   desc "Flexible forwarding DNS client"
   homepage "https://github.com/IrineSistiana/mosdns"
-  version "5.1.3"
+  version "5.2.1"
   license "GPL-3.0"
   revision 0
 
