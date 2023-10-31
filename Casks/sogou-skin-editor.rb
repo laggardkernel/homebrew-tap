d5ad47537e260dc9452f912fe1243ff62fae12e6
laggardkernel <laggardkernel@gmail.com>
2023-10-31 15:58:23 +0800

    feat: new cask sogou-skin-editor 1.0.0

---
+++ Casks/sogou-skin-editor.rb
@@ -0,0 +1,26 @@
+cask "sogou-skin-editor" do
+  version "1.0.0"
+
+  url "https://pinyin.sogou.com/mac/softdown.php?r=skineditor"
+  name "SGSkinEditor"
+  name "搜狗 Mac 皮肤编辑器"
+  desc "Skin editor app for sougou input method on macOS"
+  homepage "https://pinyin.sogou.com/mac/skineditor.php"
+
+  livecheck do
+    url "https://pinyin.sogou.com/mac/skineditor.php"
+    strategy :page_match
+    regex(%r{<span class="post_type">[^<]+for Mac (\d+(?:\.\d+)+).*})
+  end
+
+  # auto_updates false
+  # depends_on macos: ">= :yosemite"
+
+  app "SGSkinEditor.app"
+
+  zap trash: [
+    "~/Library/Caches/com.sogou.SGSkinEditor",
+    "~/Library/Preferences/com.sogou.SGSkinEditor.plist",
+    "~/Library/Saved Application State/com.sogou.SGSkinEditor.savedState",
+  ]
+end
