4880008c7bd60138ef2c5e437a42bcb74dc14be8
laggardkernel <laggardkernel@gmail.com>
2023-12-11 14:13:32 +0800

    Upgrade nali to 0.8.1

--- Formula/nali-bin.rb
+++ Formula/nali-bin.rb
@@ -1,7 +1,7 @@
 class NaliBin < Formula
   desc "Offline tool for querying IP geographic information and CDN provider"
   homepage "https://github.com/zu1k/nali"
-  version "0.8.0"
+  version "0.8.1"
   license "MIT"
 
   head do
