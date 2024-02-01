a59dfcad871fe1ad4aa9c53147c22967ea7a96b5
laggardkernel <laggardkernel@gmail.com>
2024-02-01 11:47:50 +0800

    Upgrade mongosh to 2.1.3

--- Formula/mongosh-bin.rb
+++ Formula/mongosh-bin.rb
@@ -1,7 +1,7 @@
 class MongoshBin < Formula
   desc "MongoDB Shell to connect, configure, query, and work with your MongoDB database"
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  version "2.1.1"
+  version "2.1.3"
   license "Apache-2.0"
 
   livecheck do
