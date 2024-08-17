532ac1a9fac3f1d1b356e751c083d6d25deaee6b
laggardkernel <laggardkernel@gmail.com>
2024-08-17 21:45:51 +0800

    Weekly version bump up

--- Formula/mosdns.rb
+++ Formula/mosdns.rb
@@ -1,7 +1,7 @@
 class Mosdns < Formula
   desc "Flexible forwarding DNS client"
   homepage "https://github.com/IrineSistiana/mosdns"
-  version "5.3.1"
+  version "5.3.3"
   license "GPL-3.0"
   revision 0
 
