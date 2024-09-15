87a75da514201c345d14d2bc37ba0dc8e31d2f81
laggardkernel <laggardkernel@gmail.com>
2024-09-15 15:06:14 +0800

    Remove curl-options, update mongosh to 2.3.1, lua-language-server

--- Formula/lua-language-server-bin.rb
+++ Formula/lua-language-server-bin.rb
@@ -1,7 +1,7 @@
 class LuaLanguageServerBin < Formula
   desc "Language Server for Lua and coded by Lua, the sumneko.lua ext for VSCode"
   homepage "https://github.com/sumneko/lua-language-server"
-  version "3.10.5"
+  version "3.10.6"
   license "MIT"
 
   livecheck do
