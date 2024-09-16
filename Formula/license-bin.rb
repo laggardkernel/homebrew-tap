a03c6acfbf4fd322ce3581405a6633a3f1a210be
laggardkernel <laggardkernel@gmail.com>
2024-09-16 14:53:21 +0800

    feat(license): formula license-bin

---
+++ Formula/license-bin.rb
@@ -0,0 +1,62 @@
+class LicenseBin < Formula
+  desc "Command-line license text generator"
+  homepage "https://github.com/nishanths/license"
+  version "5.0.4"
+  license "MIT"
+  head "https://github.com/nishanths/license.git"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/nishanths/license/releases/download/v#{version}/license-v#{version}-darwin-arm64"
+  elsif OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/nishanths/license/releases/download/v#{version}/license-v#{version}-darwin-amd64"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/nishanths/license/releases/download/v#{version}/license-v#{version}-linux-amd64"
+  elsif OS.linux? && Hardware::CPU.arm? && (Hardware::CPU.is-64-bit?)
+    url "https://github.com/nishanths/license/releases/download/v#{version}/license-v#{version}-linux-arm64"
+  end
+
+  def install
+    bin.install Dir.glob("license*")[0] => "license"
+  end
+
+  test do
+    help_output = shell_output("#{bin}/license -list")
+    assert_match "agpl-3.0      (GNU Affero General Public License v3.0)", help_output
+
+    (testpath/"LICENSE.test").write <<~EOS
+      BSD 2-Clause License
+
+      Copyright (c) 2009-present, Homebrew contributors
+      All rights reserved.
+
+      Redistribution and use in source and binary forms, with or without
+      modification, are permitted provided that the following conditions are met:
+
+      1. Redistributions of source code must retain the above copyright notice, this
+         list of conditions and the following disclaimer.
+
+      2. Redistributions in binary form must reproduce the above copyright notice,
+         this list of conditions and the following disclaimer in the documentation
+         and/or other materials provided with the distribution.
+
+      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+      AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+      IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+      DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+      FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+      DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+      SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+      CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+      OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    EOS
+
+    system bin/"license", "-o", "LICENSE.txt", "-n", "Homebrew contributors", "-y", "2009-present", "bsd-2-clause"
+    assert_match File.read("LICENSE.test"), File.read("LICENSE.txt")
+  end
+end
