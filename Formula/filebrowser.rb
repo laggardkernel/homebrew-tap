6295a43de4a65babb0821775c3b8b0b228141b77
laggardkernel <laggardkernel@gmail.com>
2024-10-10 17:07:32 +0800

    Upgrade filebrowser to 2.31.2

--- Formula/filebrowser.rb
+++ Formula/filebrowser.rb
@@ -1,7 +1,7 @@
 class Filebrowser < Formula
   desc "Provides a file managing interface within a specified directory"
   homepage "https://filebrowser.org"
-  version "2.31.1"
+  version "2.31.2"
   license "MIT"
 
   head do
