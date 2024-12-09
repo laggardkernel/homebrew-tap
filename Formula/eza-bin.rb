2e07b8334b65f9e1255f3f87cd15859211e3a3e7
laggardkernel <laggardkernel@gmail.com>
2024-12-09 11:19:58 +0800

    Weekly version bump up

--- Formula/eza-bin.rb
+++ Formula/eza-bin.rb
@@ -1,7 +1,7 @@
 class EzaBin < Formula
   desc "Modern, maintained replacement for ls"
   homepage "https://github.com/eza-community/eza"
-  version "0.20.10"
+  version "0.20.11"
   resource_version=version.to_s
   license "EUPL-1.2"
 
