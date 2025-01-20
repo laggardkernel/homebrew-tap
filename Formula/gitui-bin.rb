764283daf3b77e94e3b9a21d5655c2adab66fb6f
laggardkernel <laggardkernel@gmail.com>
2025-01-20 11:51:11 +0800

    Weekly version bump up

--- Formula/gitui-bin.rb
+++ Formula/gitui-bin.rb
@@ -1,7 +1,7 @@
 class GituiBin < Formula
   desc "Blazing fast terminal-ui for git written in rust"
   homepage "https://github.com/extrawurst/gitui"
-  version "0.26.3"
+  version "0.27.0"
   license "MIT"
   revision 1
 
