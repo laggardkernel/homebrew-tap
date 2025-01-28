c6f9f332b7370d5ff3f6ef75360557add34f137e
laggardkernel <laggardkernel@gmail.com>
2025-01-28 20:08:57 +0800

    Add old version orbstack for monterey

---
+++ Casks/orbstack-versioned.rb
@@ -0,0 +1,50 @@
+cask "orbstack-versioned" do
+  arch arm: "arm64", intel: "amd64"
+
+  # https://docs.orbstack.dev/release-notes#v1-8-0
+  # on_monterey do
+  version "1.7.5_18165"
+  sha256 arm:   "b9a740f932ff2609530cc12032016e5d7977074ae10e642bd1f7d49c65490b06",
+         intel: "67ec06fc9ea26496cda033524049200b28da0ae8cae8744202368385755d14b0"
+
+  url "https://cdn-updates.orbstack.dev/#{arch}/OrbStack_v#{version}_#{arch}.dmg"
+  name "OrbStack"
+  desc "Replacement for Docker Desktop"
+  homepage "https://orbstack.dev/"
+
+  livecheck do
+    skip "Legacy version"
+  end
+
+  auto_updates true
+  depends_on macos: "== :monterey"
+
+  app "OrbStack.app"
+  binary "#{appdir}/OrbStack.app/Contents/MacOS/bin/orb"
+  binary "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl"
+
+  postflight do
+    system_command "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl",
+                   args: ["_internal", "brew-postflight"]
+  end
+
+  uninstall script: {
+    executable: "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl",
+    args:       ["_internal", "brew-uninstall"],
+  }
+
+  zap trash: [
+        "~/.orbstack",
+        "~/Library/Caches/dev.kdrag0n.MacVirt",
+        "~/Library/HTTPStorages/dev.kdrag0n.MacVirt",
+        "~/Library/HTTPStorages/dev.kdrag0n.MacVirt.binarycookies",
+        "~/Library/Preferences/dev.kdrag0n.MacVirt.plist",
+        "~/Library/Saved Application State/dev.kdrag0n.MacVirt.savedState",
+        "~/Library/WebKit/dev.kdrag0n.MacVirt",
+      ],
+      rmdir: "~/OrbStack"
+
+  caveats <<~EOS
+    Open the OrbStack app to finish setup.
+  EOS
+end
