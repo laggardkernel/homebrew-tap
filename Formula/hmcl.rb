f742adb382dc56be4f95fdc78998b79ce0425045
laggardkernel <laggardkernel@gmail.com>
2025-03-09 15:27:22 +0800

    Weekly version bump up

--- Formula/hmcl.rb
+++ Formula/hmcl.rb
@@ -3,7 +3,7 @@ class Hmcl < Formula
   homepage "https://hmcl.huangyuhui.net/"
   # https://github.com/huanghongxun/HMCL
   # rubocop: disable all
-  version "3.6.11.270"
+  version "3.6.11.271"
   url "https://github.com/huanghongxun/HMCL/releases/download/v#{version}/HMCL-#{version}.jar"
   # WARN: network quality of the ci site is unreliable, fetch releases from
   # github-actions instead.
