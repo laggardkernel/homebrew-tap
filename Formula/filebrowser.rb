7a15b61ba00ee58605ecc733229d944031ac8d00
laggardkernel <laggardkernel@gmail.com>
2025-09-30 19:48:34 +0800

    Weekly version bump up

--- Formula/filebrowser.rb
+++ Formula/filebrowser.rb
@@ -1,7 +1,7 @@
 class Filebrowser < Formula
   desc "Provides a file managing interface within a specified directory"
   homepage "https://filebrowser.org"
-  version "2.43.0"
+  version "2.44.0"
   license "MIT"
 
   head do
