8dbec4c8dac312020854bd2c6688564020be35d0
laggardkernel <laggardkernel@gmail.com>
2025-10-01 13:40:11 +0800

    feat(mamba): replace conda-standalone with micromamba

--- Formula/conda-standalone.rb
+++ Formula/conda-standalone.rb
@@ -10,31 +10,30 @@ class CondaStandalone < Formula
   end
 
   livecheck do
-    url "https://anaconda.org/anaconda/conda-standalone/files"
-    regex(%r{href=.*?/osx-(.*?)/conda[_-]standalone[_-]v?(\d+(?:\.\d+)+)[_-](.+?)\.tar.bz2}i)
-    strategy :page_match do |page, regex|
-      version_map = Hash.new { |hash, key| hash[key] = {} }
-
-      page.scan(regex).each do |match|
-        arch, version, build = match
-        next if version_map[version].key?(arch) && compare_builds(version_map[version][arch], build)
-
-        version_map[version][arch] = build
-      end
-
-      version_map.map do |version, builds|
-        # Ensure '64' is first, then 'arm64', followed by any other architectures
-        ordered_builds = ["64", "arm64"].map { |arch| builds.fetch(arch, "-") }
-        ordered_builds.map! { |build| build.empty? ? "-" : build }
-        ordered_builds += builds.except("64", "arm64").values
-        "#{version},#{ordered_builds.join(",")}"
-      end
-    end
-
-    def compare_builds(build1, build2)
-      build1.split("_").last.to_i > build2.split("_").last.to_i
-    end
+    skip "Unmaintained by this tap"
+    # url "https://anaconda.org/anaconda/conda-standalone/files"
+    # regex(%r{href=.*?/osx-(.*?)/conda[_-]standalone[_-]v?(\d+(?:\.\d+)+)[_-](.+?)\.tar.bz2}i)
+    # strategy :page_match do |page, regex|
+    #   version_map = Hash.new { |hash, key| hash[key] = {} }
+    #   page.scan(regex).each do |match|
+    #     arch, version, build = match
+    #     next if version_map[version].key?(arch) && compare_builds(version_map[version][arch], build)
+    #     version_map[version][arch] = build
+    #   end
+    #   version_map.map do |version, builds|
+    #     # Ensure '64' is first, then 'arm64', followed by any other architectures
+    #     ordered_builds = ["64", "arm64"].map { |arch| builds.fetch(arch, "-") }
+    #     ordered_builds.map! { |build| build.empty? ? "-" : build }
+    #     ordered_builds += builds.except("64", "arm64").values
+    #     "#{version},#{ordered_builds.join(",")}"
+    #   end
+    # end
+    # def compare_builds(build1, build2)
+    #   build1.split("_").last.to_i > build2.split("_").last.to_i
+    # end
   end
+
+  disable! date: "2025-10-01", because: "is unmaintained by this tap"
 
   # osx-arm64 builds are provided >= 23.11.0-
   # Only support macOS temporarily, cause the version strings are different for
---
+++ Formula/micromamba-bin.rb
@@ -0,0 +1,58 @@
+class MicromambaBin < Formula
+  desc "Fast, robust, and cross-platform package manager"
+  homepage "https://mamba.readthedocs.io/en/latest/"
+  # homepage "https://github.com/mamba-org/mamba"
+  version "2.3.2-0"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://github.com/mamba-org/micromamba-releases"
+    strategy :github_releases
+    regex(/v?(\d+(?:\.\d+)+(-\d+)?)/i)
+  end
+
+  os_name = OS.mac? ? "osx" : "linux"
+  if OS.mac?
+    os_name = "osx"
+    cpu_arch = Hardware::CPU.intel? ? "64": "arm64"
+  else
+    os_name = "linux"
+    if Hardware::CPU.ppc64le?
+      cpu_arch = "ppc64le"
+    elsif Hardware::CPU.arm?
+      cpu_arch = "aarch64"
+    else
+      cpu_arch = "64"
+    end
+  end
+  # micromamba-{os_name}-{cpu_arch}.tar.bz2
+  name_middle = [os_name, cpu_arch].reject(&:empty?).join('-')
+  url "https://github.com/mamba-org/micromamba-releases/releases/download/#{version}/micromamba-#{name_middle}.tar.bz2"
+
+  def install
+    bin.install "bin/micromamba"
+    # Upstream chooses names based on static or dynamic linking,
+    # but as of 2.0 they provide identical interfaces.
+    bin.install_symlink "micromamba" => "mamba"
+  end
+
+  def caveats
+    <<~EOS
+      Please run the following to setup your shell:
+        #{opt_bin}/mamba shell init --shell <your-shell> --root-prefix ~/mamba
+      and restart your terminal.
+    EOS
+  end
+
+  test do
+    ENV["MAMBA_ROOT_PREFIX"] = testpath.to_s
+
+    assert_match version.to_s, shell_output("#{bin}/mamba --version").strip
+    assert_match version.to_s, shell_output("#{bin}/micromamba --version").strip
+
+    # Using 'xtensor' (header-only package) to avoid "broken pipe" codesigning issue
+    # encountered on macOS 13-arm and 14-arm during CI.
+    system bin/"mamba", "create", "-n", "test", "xtensor", "-y", "-c", "conda-forge"
+    assert_path_exists testpath/"envs/test/include/xtensor.hpp"
+  end
+end
