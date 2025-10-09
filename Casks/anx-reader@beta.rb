12eda490e742f8e5eab369f74e2f821fde9e0dc7
laggardkernel <laggardkernel@gmail.com>
2025-10-09 10:28:37 +0800

    Upgrade anx-reader beta to 1.8.2-5

--- Casks/anx-reader@beta.rb
+++ Casks/anx-reader@beta.rb
@@ -1,5 +1,5 @@
 cask "anx-reader@beta" do
-  version "1.8.2-4"
+  version "1.8.2-5"
 
   url "https://github.com/Anxcye/anx-reader/releases/download/beta-#{version}/Anx-Reader-macos-beta-#{version.split("-").first}.zip"
   name "Anx Reader"
