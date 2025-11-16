d8c0073ea37f25b1f6996bce6071f77848aba8da
laggardkernel <laggardkernel@gmail.com>
2025-11-16 22:15:56 +0800

    New formula librime-cloud

---
+++ Formula/librime-cloud-bin.rb
@@ -0,0 +1,35 @@
+class LibrimeCloudBin < Formula
+  desc "Rime Cloud Pinyin Plugin"
+  homepage "https://github.com/hchunhui/librime-cloud"
+  version 'latest'
+
+  os_name = OS.mac? ? "macos" : "linux"
+  cpu_arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
+  basename = "#{os_name}-#{cpu_arch}-lua5.4.tar.gz"
+  url "https://github.com/hchunhui/librime-cloud/releases/download/continuous/#{basename}"
+
+  def install
+    lib_dst = lib/"librime-cloud"
+    mkdir_p lib_dst
+    lib_dst.install Dir["out-*/*"]
+
+    share_dst = share/"librime-cloud"
+    mkdir_p share_dst
+    share_dst.install Dir["scripts/*"]
+
+    prefix.install_metafiles
+  end
+
+  def caveats
+    <<~EOS
+      Follow instructions from https://github.com/hchunhui/librime-cloud/issues/17
+      for setup. Disable qurantine for the libs manually:
+
+      sudo xattr -dr com.apple.quarantine #{lib}/librime-cloud/*.so
+
+      Add the lib path into rime.lua:
+
+      package.cpath = package.cpath .. ";#{lib}/librime-cloud/?.so"
+    EOS
+  end
+end
