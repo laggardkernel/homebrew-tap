f28ab16d441595b4fbe51e13d5cf6995f5b13852
laggardkernel <laggardkernel@gmail.com>
2025-11-16 13:43:03 +0800

    New cask fcitx-rime 0.2.8

---
+++ Casks/fcitx-rime.rb
@@ -0,0 +1,27 @@
+cask "fcitx-rime" do
+  version "0.2.8"
+
+  url "https://github.com/fcitx-contrib/fcitx5-macos-installer/releases/download/#{version}/Fcitx5-Rime.zip"
+  name "Fcitx"
+  desc "Fcitx5 input method framework"
+  homepage "https://github.com/fcitx-contrib/fcitx5-macos"
+
+  livecheck do
+    url "https://github.com/fcitx-contrib/fcitx5-macos-installer"
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  installer manual: "Fcitx5Installer.app"
+
+  # /Library/Input Methods/Fcitx5.app/Contents/Resources/uninstall.sh "$USER"
+  uninstall quit:   "org.fcitx.inputmethod.Fcitx5",
+            delete: "/Library/Input Methods/Fcitx5.app" # delete but not pkgutil
+
+  zap trash: "~/Library/fcitx5",
+      rmdir: [
+        "~/.config/fcitx5",
+        "~/.local/share/fcitx5", # rime
+      ]
+end
