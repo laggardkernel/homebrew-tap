93f02f334b207e22300ebe515d7de39041cf5140
laggardkernel <laggardkernel@gmail.com>
2025-11-24 17:12:07 +0800

    Upgrade anx-reader to 1.10.0

--- Casks/anx-reader.rb
+++ Casks/anx-reader.rb
@@ -1,5 +1,5 @@
 cask "anx-reader" do
-  version "1.9.2"
+  version "1.10.0"
 
   url "https://github.com/Anxcye/anx-reader/releases/download/v#{version}/Anx-Reader-macos-#{version}.zip"
   name "Anx Reader"
