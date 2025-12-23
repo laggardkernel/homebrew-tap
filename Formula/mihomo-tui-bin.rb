aa9c57fa9df4c895858ceab2bf7bb02add6cda96
laggardkernel <laggardkernel@gmail.com>
2025-12-23 14:24:59 +0800

    feat(mihomo-tui): tui dashboard

---
+++ Formula/mihomo-tui-bin.rb
@@ -0,0 +1,25 @@
+class MihomoTuiBin < Formula
+  desc "Simple TUI dashboard for monitoring and managing Mihomo via its REST API"
+  homepage "https://github.com/potoo0/mihomo-tui"
+  version "0.2.1"
+  license "MIT"
+  revision 0
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  os_name = OS.mac? ? "macOS" : "Linux-gnu"
+  if OS.mac?
+    cpu_arch = "arm64"
+  else
+    cpu_arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
+  end
+  basename = "mihomo-tui-#{os_name}-#{cpu_arch}.tar.gz"
+  url "https://github.com/potoo0/mihomo-tui/releases/download/v#{version}/#{basename}"
+
+  def install
+    bin.install "mihomo-tui"
+  end
+end
