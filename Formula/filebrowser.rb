8a98c077437bc35462760ff7112a1811657d4de5
laggardkernel <laggardkernel@gmail.com>
2026-02-27 23:24:02 +0800

    WIP

--- Formula/filebrowser.rb
+++ Formula/filebrowser.rb
@@ -1,7 +1,7 @@
 class Filebrowser < Formula
   desc "Provides a file managing interface within a specified directory"
   homepage "https://filebrowser.org"
-  version "2.60.0"
+  version "2.61.2"
   license "MIT"
 
   head do
