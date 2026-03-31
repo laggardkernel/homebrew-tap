13516f26d2fbde712cc50f79e7f16caa0e18b72a
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-03-31 10:45:33 +0800

    Regular version bump up

--- Formula/axonhub-bin.rb
+++ Formula/axonhub-bin.rb
@@ -1,7 +1,7 @@
 class AxonhubBin < Formula
   desc "OpenAI-compatible API gateway for coding agents and LLM clients"
   homepage "https://github.com/looplj/axonhub"
-  version "0.9.23"
+  version "0.9.30"
   license "Apache-2.0"
 
   os_name = OS.mac? ? "darwin" : "linux"
