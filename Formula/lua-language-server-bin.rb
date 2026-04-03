afc90421fc6971aa138131bbc60c09de3bd32302
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-04-04 00:58:24 +0800

    WIP 4

--- Formula/lua-language-server-bin.rb
+++ Formula/lua-language-server-bin.rb
@@ -1,7 +1,7 @@
 class LuaLanguageServerBin < Formula
   desc "Language Server for Lua and coded by Lua, the sumneko.lua ext for VSCode"
   homepage "https://github.com/sumneko/lua-language-server"
-  version "3.17.1"
+  version "3.18.0"
   license "MIT"
 
   livecheck do
