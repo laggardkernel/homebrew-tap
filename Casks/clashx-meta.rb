c43a5746e5866e1db6c24b1e08015622b0734c97
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-07-01 23:25:11 +0800

    Regular version bump up

--- Casks/clashx-meta.rb
+++ Casks/clashx-meta.rb
@@ -1,5 +1,5 @@
 cask "clashx-meta" do
-  version "1.4.41"
+  version "1.4.42"
 
   url "https://github.com/MetaCubeX/ClashX.Meta/releases/download/v#{version}/ClashX.Meta.zip"
   name "ClashX Meta"
--- Formula/asdf-bin.rb
+++ Formula/asdf-bin.rb
@@ -1,7 +1,7 @@
 class AsdfBin < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Erlang & more"
   homepage "https://asdf-vm.com/"
-  version "0.19.0"
+  version "0.20.0"
   license "MIT"
   revision 0
 
