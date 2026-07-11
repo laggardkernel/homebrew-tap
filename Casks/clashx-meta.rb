db27c68304a3e15a4ad27844ee9eb0e07acdafe5
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-07-11 23:36:06 +0800

    Regular version bump up

--- Casks/clashx-meta.rb
+++ Casks/clashx-meta.rb
@@ -1,5 +1,5 @@
 cask "clashx-meta" do
-  version "1.4.42"
+  version "1.4.43"
 
   url "https://github.com/MetaCubeX/ClashX.Meta/releases/download/v#{version}/ClashX.Meta.zip"
   name "ClashX Meta"
--- Formula/adguardhome.rb
+++ Formula/adguardhome.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Adguardhome < Formula
   desc "Network-wide ads & trackers blocking DNS server"
   homepage "https://github.com/AdguardTeam/AdGuardHome"
-  version "0.107.77"
+  version "0.107.78"
   license "GPL-3.0"
 
   livecheck do
