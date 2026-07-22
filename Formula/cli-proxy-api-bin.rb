7d157063a7cd0d39802661a879f013dd4b86f978
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-07-22 11:56:06 +0800

    Regular version bump up

--- Formula/cli-proxy-api-bin.rb
+++ Formula/cli-proxy-api-bin.rb
@@ -1,7 +1,7 @@
 class CliProxyApiBin < Formula
   desc "Wrap Gemini CLI, Codex, Claude Code, Qwen Code as an API service"
   homepage "https://github.com/router-for-me/CLIProxyAPI"
-  version "7.2.91"
+  version "7.2.102"
   license "MIT"
 
   os_name = OS.mac? ? "darwin" : "linux"
--- Formula/eza-bin.rb
+++ Formula/eza-bin.rb
@@ -1,7 +1,7 @@
 class EzaBin < Formula
   desc "Modern, maintained replacement for ls"
   homepage "https://github.com/eza-community/eza"
-  version "0.23.4"
+  version "0.23.5"
   resource_version=version.to_s
   license "EUPL-1.2"
 
