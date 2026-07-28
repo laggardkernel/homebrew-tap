2e31a8e1850c9a2d7ac7ab06ee71fc8dd252fc1e
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-07-28 10:53:04 +0800

    Regular version bump up

--- Formula/filebrowser.rb
+++ Formula/filebrowser.rb
@@ -1,7 +1,7 @@
 class Filebrowser < Formula
   desc "Provides a file managing interface within a specified directory"
   homepage "https://filebrowser.org"
-  version "2.63.21"
+  version "2.63.23"
   license "MIT"
 
   head do
