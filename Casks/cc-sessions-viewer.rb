cfafc606b05732bc211ee1f41459dce89fb32656
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-08-27 00:19:25 +0800

    Regular version bump up

--- Casks/cc-sessions-viewer.rb
+++ Casks/cc-sessions-viewer.rb
@@ -1,5 +1,5 @@
 cask "cc-sessions-viewer" do
-  version "0.3.21"
+  version "0.3.26"
   sha256 :no_check
 
   url "https://github.com/jerrywu001/cc-sessions-viewer/releases/download/v#{version}/Sessions.Viewer_#{version}_universal.dmg"
