73c43b6cfbc650e298bd1249f79e4aa82e40de30
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-09-09 16:46:14 +0800

    WIP 1

--- Formula/herdr-bin.rb
+++ Formula/herdr-bin.rb
@@ -1,7 +1,7 @@
 class HerdrBin < Formula
   desc "Agent multiplexer that lives in your terminal"
   homepage "https://herdr.dev"
-  version "0.8.2"
+  version "0.9.0"
   license "Apache-2.0"
 
   os_name = OS.mac? ? "macos" : "linux"
