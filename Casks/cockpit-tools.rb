29592cf8822fdb79425eb9d73270d27811beedc9
laggardkernel <12206611+laggardkernel@users.noreply.github.com>
2026-09-14 00:29:26 +0800

    feat: new cask cockpit tools 1.3.51

---
+++ Casks/cockpit-tools.rb
@@ -0,0 +1,35 @@
+cask "cockpit-tools" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "1.3.51"
+  sha256 :no_check
+
+  url "https://github.com/jlcodes99/cockpit-tools/releases/download/v#{version}/Cockpit.Tools_#{version}_#{arch}.dmg"
+  name "Cockpit Tools"
+  desc "Account manager for AI IDEs and CLI tools"
+  homepage "https://github.com/jlcodes99/cockpit-tools"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on :macos
+
+  app "Cockpit Tools.app"
+
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "Cockpit Tools.app"], base: :appdir,
+        must_succeed: false, print_stderr: false
+  end
+
+  uninstall quit: "com.jlcodes.cockpit-tools"
+
+  zap trash: [
+    "~/Library/Application Support/com.jlcodes.cockpit-tools",
+    "~/Library/Caches/com.jlcodes.cockpit-tools",
+    "~/Library/Preferences/com.jlcodes.cockpit-tools.plist",
+    "~/Library/Saved Application State/com.jlcodes.cockpit-tools.savedState",
+  ]
+end
